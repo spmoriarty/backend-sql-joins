fd484d10ec331ba2996e73089eb1d1daea3b30fc
Sean Moriarty <spmoriarty1@gmail.com>
2022-08-04 15:08:47 -0700

    working test 14

--- exercise_14/exercise_14.sql
+++ exercise_14/exercise_14.sql
@@ -1,2 +1,9 @@
 -- the average film length by category
 -- include the category name and avg length
+SELECT AVG (length), name
+FROM film
+
+INNER JOIN film_category ON film_category.film_id = film.film_id
+INNER JOIN category ON category.category_id = film_category.category_id
+GROUP BY category.name
+LIMIT 16
